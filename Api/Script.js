6ddda79c79ffb5887b9d1700c105bed4e322f693
Feyk <feykhere@gmail.com>
2025-05-02 15:39:52 +0530

    Update Script.js

--- Api/Script.js
+++ Api/Script.js
@@ -1,1 +1,7 @@
+export default function handler(req, res) {
+  // Set the content type as plain text
+  res.setHeader("Content-Type", "text/plain");
 
+  // Send the Lua loadstring for your private script
+  res.send(`loadstring("https://raw.githubusercontent.com/FeykHere/feykhub_Bladeball/main/feykhubbladeball.txt")()`);
+}
